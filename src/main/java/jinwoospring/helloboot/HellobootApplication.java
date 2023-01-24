8cce4ada76f032f6a8e181f42dbaca0b0ec69805
Jinwoo Park <zvzv9808@gmail.com>
2023-01-24 22:29:19 +0900

    Start Servlet Container - Tomcat

--- src/main/java/jinwoospring/helloboot/HellobootApplication.java
+++ src/main/java/jinwoospring/helloboot/HellobootApplication.java
@@ -1,9 +1,15 @@
 package jinwoospring.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless");
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start(); //Tomcat Start
 	}
 
 }
